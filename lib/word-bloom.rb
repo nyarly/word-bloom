62ee8aeb0a69752b748f287ad98bd253dc8a9bd7
Judson <nyarly@gmail.com>
2012-09-08 01:38:23 -0700

    Text encoding still annoys

--- lib/word-bloom.rb
+++ lib/word-bloom.rb
@@ -1,8 +1,16 @@
-
 require 'bloominsimple'
 require 'digest/sha1'
 
 class WordBloom
-  HASHER = lambda { |item| Digest::SHA1.digest(item.encode('UTF-8', :invalid => :replace).downcase.strip).unpack("VV") }
+  HASHER = lambda do |item|
+    begin
+      item = item.encode("UTF-16LE", :invalid => :replace, :undef => :replace, :replace => "").encode("UTF-8")
+      Digest::SHA1.digest(item.downcase.strip).unpack("VV")
+    rescue ArgumentError => ex
+      p __ENCODING__
+      p ex.message, item
+      raise
+    end
+  end
   LANGUAGE_DIR_PATH = File.expand_path("../../lang", __FILE__)
 end
--- spec/filter-builder.rb
+++ spec/filter-builder.rb
@@ -2,8 +2,14 @@ require 'word-bloom'
 require 'word-bloom/filter-builder'
 
 describe WordBloom::FilterBuilder do
-  it "should ingest a file" do
+  it "should ingest pinyin" do
     builder = WordBloom::FilterBuilder.new(File::expand_path("../../wordlists/pinyin", __FILE__))
+    filter = builder.filter_from_dictionary
+    filter.should be_an_instance_of(BloominSimple)
+  end
+
+  it "should ingest italian" do
+    builder = WordBloom::FilterBuilder.new(File::expand_path("../../wordlists/italian", __FILE__))
     filter = builder.filter_from_dictionary
     filter.should be_an_instance_of(BloominSimple)
   end
